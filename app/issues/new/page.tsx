205fc79f91c51b5d33983c78848563ce569a876b
A3thyr <a3therys@gmail.com>
2024-04-11 17:09:18 +0300

    feat: disabled ssr for markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
+import ErrorMessage from "@/app/components/ErrorMessage";
+import dynamic from "next/dynamic";
+import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
