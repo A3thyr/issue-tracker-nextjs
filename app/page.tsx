2e01cd9da194f9acf11a2f0f08080f0797566070
A3thyr <a3therys@gmail.com>
2024-04-15 13:11:32 +0300

    feat: added layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <h1>Yo</h1>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
 }
